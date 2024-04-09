c9e52e0e718ca9eb3f58ed12568733dd4d19dfd7
Yevhenii Pozniak <jekamanu@gmail.com>
2024-04-09 14:27:26 +0300

    update

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,16 +1,14 @@
 import AuthNav from "../AuthNav/AuthNav";
 import Navigation from "../Navigation/Navigation";
 import UserMenu from "../UserMenu/UserMenu";
-import { Container, Header } from "./AppBar.styled";
+import { Header } from "./AppBar.styled";
 
 export default function AppBar() {
   const isLoggedIn = false;
   return (
-    <Container>
-      <Header>
-        <Navigation />
-        {isLoggedIn ? <UserMenu /> : <AuthNav />}
-      </Header>
-    </Container>
+    <Header>
+      <Navigation />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
+    </Header>
   );
 }
